997f39c5dba125b88f822ca1b5077346831231b3
Matias Lenci <matiaslenci@gmail.com>
2023-05-27 14:05:47 -0300

    add cards con animaciones

--- src/app/modules/home/components/card-shop/card-shop.component.ts
+++ src/app/modules/home/components/card-shop/card-shop.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class CardShopComponent {
 
+  ropas:any= [{
+    img:"https://i.pinimg.com/564x/b4/09/44/b40944ca15d3b7d043beb61c7495f6f6.jpg",
+    titulo:'Remera pink',
+    stock:true,
+    price:5000,
+
+  },
+  {
+    img:"https://i.pinimg.com/564x/b4/09/44/b40944ca15d3b7d043beb61c7495f6f6.jpg",
+    titulo:'Remera pink',
+    stock:true,
+    price:4000,
+
+  },
+  {
+    img:"https://i.pinimg.com/564x/b4/09/44/b40944ca15d3b7d043beb61c7495f6f6.jpg",
+    titulo:'Remera pink',
+    stock:true,
+    price:3000,
+
+  },{
+    img:"https://i.pinimg.com/564x/b4/09/44/b40944ca15d3b7d043beb61c7495f6f6.jpg",
+    titulo:'Remera pink',
+    stock:true,
+    price:3000,
+
+  },{
+    img:"https://i.pinimg.com/564x/b4/09/44/b40944ca15d3b7d043beb61c7495f6f6.jpg",
+    titulo:'Remera pink',
+    stock:true,
+    price:3000,
+
+  }]
 }
