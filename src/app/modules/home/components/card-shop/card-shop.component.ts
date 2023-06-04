c5a5bc62f1a81cee063aeff2a4abdb79f7f340eb
Matias Lenci <matiaslenci@gmail.com>
2023-06-04 11:18:41 -0300

    add alt a img

--- src/app/modules/home/components/card-shop/card-shop.component.ts
+++ src/app/modules/home/components/card-shop/card-shop.component.ts
@@ -9,39 +9,49 @@ export class CardShopComponent {
   ropas: any = [
     {
       img: 'https://i.pinimg.com/564x/b4/09/44/b40944ca15d3b7d043beb61c7495f6f6.jpg',
-      modelo:'https://i.pinimg.com/564x/69/9f/df/699fdf36a1c3dd49079f0b67dc565c1b.jpg',
+      altProducto: '',
+      modelo:
+        'https://i.pinimg.com/564x/69/9f/df/699fdf36a1c3dd49079f0b67dc565c1b.jpg',
       titulo: 'Remera pink',
       stock: true,
       price: 5000,
     },
     {
       img: 'https://i.pinimg.com/564x/e3/f2/ca/e3f2ca2bead90e4b47c99e526be3e699.jpg',
+      altProducto: '',
       modelo:
         'https://i.pinimg.com/564x/69/9f/df/699fdf36a1c3dd49079f0b67dc565c1b.jpg',
+      altModelo: '',
       titulo: 'Remera pink',
       stock: true,
       price: 4000,
     },
     {
       img: 'https://i.pinimg.com/564x/b4/09/44/b40944ca15d3b7d043beb61c7495f6f6.jpg',
+      altProducto: '',
       modelo:
         'https://i.pinimg.com/564x/69/9f/df/699fdf36a1c3dd49079f0b67dc565c1b.jpg',
+      altModelo: '',
       titulo: 'Remera pink',
       stock: true,
       price: 3000,
     },
     {
       img: 'https://i.pinimg.com/564x/e3/f2/ca/e3f2ca2bead90e4b47c99e526be3e699.jpg',
+      altProducto: '',
       modelo:
         'https://i.pinimg.com/564x/69/9f/df/699fdf36a1c3dd49079f0b67dc565c1b.jpg',
+      altModelo: '',
       titulo: 'Remera pink',
       stock: true,
       price: 3000,
     },
     {
       img: 'https://i.pinimg.com/564x/b4/09/44/b40944ca15d3b7d043beb61c7495f6f6.jpg',
+      altProducto: '',
       modelo:
         'https://i.pinimg.com/564x/69/9f/df/699fdf36a1c3dd49079f0b67dc565c1b.jpg',
+      altModelo: '',
       titulo: 'Remera pink',
       stock: true,
       price: 3000,
